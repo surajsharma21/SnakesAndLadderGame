2a9e7a584dba827d2d1dfdefa5c088ade6748e3f
Suraj <surajshrm000@gmail.com>
2020-09-30 16:20:24 +0530

    [Suraj] Add: Welcome Message

---
+++ SnakesAndLadderGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakesAndLadderGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snakes and Ladder Problem");
+        }
+    }
+}
