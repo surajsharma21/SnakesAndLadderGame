5d511667aa3242fa620f00fb3d9634abb01e1b51
Suraj <surajshrm000@gmail.com>
2020-09-30 16:28:52 +0530

    [Suraj] Add: Game start program

--- SnakesAndLadderGame/Program.cs
+++ SnakesAndLadderGame/Program.cs
@@ -4,9 +4,14 @@ namespace SnakesAndLadderGame
 {
     class Program
     {
+        public const int START_POINT = 0;
+        public const int END_POINT = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snakes and Ladder Problem");
+            Console.WriteLine("Welcome to snakes and ladders game \nEnter player name");
+            string player1 = Console.ReadLine();
+            int playerPosition = START_POINT;
+            Console.WriteLine("Current Position : " + playerPosition);
         }
     }
 }
